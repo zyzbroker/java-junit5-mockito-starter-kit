7800e31713dcca550a15c935172d755be0446741
david <cmdy360@gmail.com>
2020-03-27 12:07:36 -0500

    add logger

--- src/main/java/com/resentek/unittest/Calculator.java
+++ src/main/java/com/resentek/unittest/Calculator.java
@@ -1,7 +1,14 @@
 package com.resentek.unittest;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Calculator {
+
+    private Logger logger = LoggerFactory.getLogger(Calculator.class);
+
     public Integer add(Integer a, Integer b){
+        logger.info("Calculator.add({},{}) returns {} --", a, b, a + b);
         return a + b;
     }
 }
--- src/test/java/com/resentek/unittest/CalculatorTestTwo.java
+++ src/test/java/com/resentek/unittest/CalculatorTestTwo.java
@@ -1,27 +1,34 @@
 package com.resentek.unittest;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class CalculatorTestTwo {
+    private Logger logger = LoggerFactory.getLogger(CalculatorTestTwo.class);
 
     @Mock
     Calculator mockedCalculator;
 
     @FastTest
     void testMockedAdd() {
+
         //GIVEN
-        when(mockedCalculator.add(1,1)).thenReturn(2);
+        logger.info("-- given --");
+        when(mockedCalculator.add(1,1)).thenReturn(3);
 
         //WHEN
         Integer result = mockedCalculator.add(1,1);
 
         //THEN
-        assertEquals(2, result, "1+1=2");
+        verify(mockedCalculator,times(1)).add(1,1);
+        assertEquals(3, result, "1+1=3");
     }
 }
